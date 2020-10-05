6dc95dfb57f3eb722a3d41173e677217e843c275
Jakub Sekula <kubasekula@seznam.cz>
2020-10-05 22:32:32 +0200

    Acquiring domain name

--- dns.cpp
+++ dns.cpp
@@ -6,6 +6,38 @@
 
 using namespace std;
 
+typedef struct {
+    uint16_t id;
+# if __BYTE_ORDER ==  __BIG_ENDIAN
+    uint16_t qr:1;
+    uint16_t opcode:4;
+    uint16_t aa:1;
+    uint16_t tc:1;
+    uint16_t rd:1;
+    uint16_t ra:1;
+    uint16_t zero:3;
+    uint16_t rcode:4;
+# elif __BYTE_ORDER == __LITTLE_ENDIAN
+    uint16_t rd:1;
+    uint16_t tc:1;
+    uint16_t aa:1;
+    uint16_t opcode:4;
+    uint16_t qr:1;
+    uint16_t rcode:4;
+    uint16_t zero:3;
+    uint16_t ra:1;
+# else
+#  error "Adjust your <bits/endian.h> defines"
+# endif
+    uint16_t qcount;    /* question count */
+    uint16_t ancount;    /* Answer record count */
+    uint16_t nscount;    /* Name Server (Autority Record) Count */ 
+    uint16_t adcount;    /* Additional Record Count */
+    uint8_t length;
+} dnshdr;
+
+// 97 - 104
+
 int main( int argc, char *argv[] ){
     
     if( argv[ 1 ] == NULL ){
@@ -20,13 +52,14 @@ int main( int argc, char *argv[] ){
     int addrlen = sizeof( my_addr );
     char buffer[ 1024 ] = {0};
 
-    memset( &my_addr, 0, sizeof( my_addr ) );
-    memset( &cliaddr, 0, sizeof( cliaddr ) );
-
-    if( ( socketfd = socket( AF_INET, SOCK_DGRAM, 0 ) ) == 0 ){
+    // TODO zkontrolovat podminku
+    if( ( socketfd = socket( AF_INET, SOCK_DGRAM, 0 ) ) < 0 ){
         cout << "Error with socket creation";
         exit( 10 );
     }
+
+    memset( &my_addr, 0, sizeof( my_addr ) );
+    memset( &cliaddr, 0, sizeof( cliaddr ) );
 
     my_addr.sin_family = AF_INET;
     my_addr.sin_addr.s_addr = INADDR_ANY;
@@ -39,15 +72,32 @@ int main( int argc, char *argv[] ){
 
     int len = sizeof( cliaddr );
 
-    while( 1 ){
-        recvfrom( socketfd, (char *)buffer, 1024, MSG_WAITALL, ( struct sockaddr *) &cliaddr, ( socklen_t *) &len );
+    recvfrom( socketfd, (char *)buffer, 1024, 0, ( struct sockaddr *) &cliaddr, ( socklen_t *) &len );
 
-        fread = read( socketfd, buffer, 1024 );
 
-        cout << buffer;
+    dnshdr* pd = ( dnshdr* ) buffer;
 
-        sendto( socketfd, "Affirmative", 10, MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len); 
+    int i = 13;
+    int last = i;
+    string domain;
 
+    while( pd->length != 0 ){
+        while( i < last + pd->length ){
+            //printf( "%c", buffer[ i ] );
+            domain = domain + buffer[ i ];
+            i++;
+        }
+        pd->length = buffer[ i ];
+        last = i + 1;
+        if( pd->length != 0 ){
+            domain = domain + ".";
+        }
     }
+
+    printf( "%s \n", domain.c_str() );
+
+    sendto( socketfd, buffer, 1024, MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len); 
+
+    return 0;
 
 }
--- makefile
+++ makefile
@@ -2,5 +2,8 @@ proj=dns
 
 make:
 	@g++ $(proj).cpp -o $(proj)
+run:
+	@g++ $(proj).cpp -o $(proj)
+	./dns 8080
 clean:
 	@rm $(proj)
